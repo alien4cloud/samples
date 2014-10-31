679cf38a70a3ea8760d41b9d59e8eb5572d5bdeb
Philippe Benoit <philippe.benoit@fastconnect.fr>
2014-10-31 18:24:15 +0100

    Add URL option on website

--- website/scripts/deploy_website.sh
+++ website/scripts/deploy_website.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-unzip $WEBFILE_ZIP -d $DOC_ROOT
+if [ -z "${WEBFILE_CURL}" ]; then
+  unzip $WEBFILE_ZIP -d $DOC_ROOT
+else
+  curl $WEBFILE_CURL
+  nameZip=${URL##*/}
+  unzip $nameZip -d $DOC_ROOT
+fi
--- website/scripts/post_config_apache.groovy
+++ website/scripts/post_config_apache.groovy
@@ -20,10 +20,13 @@ def getArtifactFrom(artifactName, String... artifacts) {
 
 def websiteRelativePath = getArtifactFrom("website_zip", args)
 def websiteAbsolutePath = "${context.serviceDirectory}/../${websiteRelativePath}"
-def docRoot = "/var/www/"
+def docRoot = "/var/www/${config.website.context_path}"
+
 builder.sequential {
   exec(executable:"${context.serviceDirectory}/scripts/deploy_website.sh", osfamily:"unix",failonerror: "true") {
     env(key:"WEBFILE_ZIP", value:websiteAbsolutePath)
     env(key:"DOC_ROOT", value:docRoot)
+    env(key:"WEBFILE_CURL", value:config.website.zip_url)
+
   }
 }

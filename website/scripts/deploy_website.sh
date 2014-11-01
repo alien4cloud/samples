388d5c5f2aaffbc81baef37e4cde23e53f61ac95
Philippe Benoit <philippe.benoit@fastconnect.fr>
2014-11-01 22:28:08 +0100

    Fix errors on the Website recipe

--- website/scripts/deploy_website.sh
+++ website/scripts/deploy_website.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ -z "${WEBFILE_CURL}" ]; then
-  unzip $WEBFILE_ZIP -d $DOC_ROOT
+if [ "$WEBFILE_URL" ]; then
+  echo "Deploy from URL"
+  wget $WEBFILE_URL
+  nameZip=${WEBFILE_URL##*/}
+  unzip -o $nameZip -d tmp
+  eval "mv -f tmp/* $DOC_ROOT"
 else
-  curl $WEBFILE_CURL
-  nameZip=${URL##*/}
-  unzip $nameZip -d $DOC_ROOT
+  echo "Deploy from artifact"
+  unzip -o $WEBFILE_ZIP -d tmp
+  eval "mv -f tmp/* $DOC_ROOT"
 fi

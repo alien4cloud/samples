d978a1d14a5d818bb54cbe8bd0e9b7534e970bc1
Philippe Benoit <philippe.benoit@fastconnect.fr>
2014-11-27 14:14:51 +0100

    Upgrade components to the wd03

--- git/scripts/installGit.sh
+++ git/scripts/installGit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Git and install..."
-sudo apt-get update && apt-get -yq install git
+sudo apt-get update || error_exit $? "Failed on: sudo apt-get update"
+sudo apt-get install -y -q git || error_exit $? "Failed on: sudo apt-get install -y -q git"
 
-# Configure git (get from ENV git_user / git_email)
-echo "Git and install..."
+echo "Configure git (get from ENV git_user / git_email)"
 /usr/bin/git config --global user.name $GIT_USER
 /usr/bin/git config --global user.email $GIT_EMAIL

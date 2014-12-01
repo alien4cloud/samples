083ca2fae132aba34a734984ad98ea1b04ab2bb1
Philippe Benoit <philippe.benoit@fastconnect.fr>
2014-12-01 11:17:58 +0100

    Resolve conflict during merge

--- mysql/scripts/mysql_start_detection.groovy
+++ mysql/scripts/mysql_start_detection.groovy
@@ -2,6 +2,6 @@ import org.cloudifysource.dsl.utils.ServiceUtils
 
 def config = new ConfigSlurper().parse(new File("${context.serviceDirectory}/service.properties").toURL())
 println "mysql_start_detection.groovy: port http=${config.mysql.port} ..."
-def isPortOccupied = ServiceUtils.isPortOccupied(Integer.parseInt(config.apache.port))
+def isPortOccupied = ServiceUtils.isPortOccupied(Integer.parseInt(config.mysql.port))
 println "mysql_start_detection.groovy: isPortOccupied http=${config.mysql.port} ... ${isPortOccupied}"
 return isPortOccupied

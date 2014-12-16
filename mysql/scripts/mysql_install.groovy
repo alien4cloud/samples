3706b6b982531d3b94f233ea144407bac717a803
Philippe Benoit <philippe.benoit@fastconnect.fr>
2014-12-16 13:59:32 +0100

    Remove an unused property in Mysql

--- mysql/scripts/mysql_install.groovy
+++ mysql/scripts/mysql_install.groovy
@@ -7,5 +7,5 @@ def builder = new AntBuilder()
 // Installing MySQL
 builder.sequential {
   echo(message:"mysql_install.groovy: Running Mysql install script from FastConnect...")
-  exec(executable:"${context.serviceDirectory}/scripts/installMysql.sh", osfamily:"unix",failonerror: "true")
+  exec(executable:"${context.serviceDirectory}/scripts/installMysql.sh", osfamily:"unix", failonerror: "true")
 }
--- mysql/scripts/mysql_start_detection.groovy
+++ mysql/scripts/mysql_start_detection.groovy
@@ -2,7 +2,7 @@ import org.cloudifysource.dsl.utils.ServiceUtils
 
 def nameMysql = args[0]
 def config = new ConfigSlurper().parse(new File("${context.serviceDirectory}/service.properties").toURL())
-println "mysql_start_detection.groovy: port http=${config[nameMysql].port} ..."
-def isPortOccupied = ServiceUtils.isPortOccupied(Integer.parseInt(config[nameMysql].port))
-println "mysql_start_detection.groovy: isPortOccupied http=${config[nameMysql].port} ... ${isPortOccupied}"
+println "mysql_start_detection.groovy: port http=${config[nameMysql].db_port} ..."
+def isPortOccupied = ServiceUtils.isPortOccupied(Integer.parseInt(config[nameMysql].db_port))
+println "mysql_start_detection.groovy: isPortOccupied http=${config[nameMysql].db_port} ... ${isPortOccupied}"
 return isPortOccupied

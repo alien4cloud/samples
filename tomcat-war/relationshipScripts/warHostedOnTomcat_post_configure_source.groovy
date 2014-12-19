97bf0a861d380411e97ca6b81ea0479be8bfb55f
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2014-12-20 00:03:35 +0100

    Tomcat Sample: - update alien-base-type version - updated custom commands scripts to use préfixed names

--- tomcat-war/relationshipScripts/warHostedOnTomcat_post_configure_source.groovy
+++ tomcat-war/relationshipScripts/warHostedOnTomcat_post_configure_source.groovy
@@ -5,16 +5,15 @@ def context = ServiceContextFactory.getServiceContext()
 
 println "warHostedOnTomcat_post_configure_source<${SOURCE}> start."
 
-def warUrl
-
 if (! war_file) return "warUrl is null. So we do nothing."
-warUrl = "${context.serviceDirectory}/../${war_file}"
+def warUrl = "${context.serviceDirectory}/../${war_file}"
 println "warHostedOnTomcat_post_configure_source<${SOURCE}>: Target: ${TARGET}:${tomcatIp}, Source: ${SOURCE}, warUrl is ${warUrl} and contextPath is ${contextPath}..."
 
-println "warHostedOnTomcat_post_configure_source<${SOURCE}> invoking updateWarOnTomcat custom command on target tomcat..."
-def service = context.waitForService(context.serviceName, 60, TimeUnit.SECONDS)
+def command = "${TARGET}_updateWarOnTomcat"
+println "warHostedOnTomcat_post_configure_source<${SOURCE}> invoking ${command} custom command on target tomcat..."
+def service = context.waitForService(TARGET_HOST, 60, TimeUnit.SECONDS)
 def currentInstance = service.getInstances().find{ it.instanceId == context.instanceId }
-currentInstance.invoke("updateWarOnTomcat", warUrl as String, contextPath as String)
+currentInstance.invoke(command, "url=${warUrl}" as String, "contextPath=${contextPath}" as String)
 println "warHostedOnTomcat_post_configure_source<${SOURCE}> end"
 
 return true
--- tomcat-war/warScripts/updateWarFile.groovy
+++ tomcat-war/warScripts/updateWarFile.groovy
@@ -1,17 +1,13 @@
 import org.cloudifysource.dsl.utils.ServiceUtils
 import java.util.concurrent.TimeUnit
 
-if(args == null || args.length < 2){
-  throw new IllegalArgumentException("UpdateWarFile command requires a contextPath and an url as arguments.")
-}
-def warUrl=args[0]
-def contextPath=args[1]
-
+assert warUrl && !warUrl.trim().isEmpty(), "requires warUrl parameter"
+def command = "${PARENT}_updateWarOnTomcat"
 println "updateWarFile.groovy: warUrl is ${warUrl} and contextPath is ${contextPath}..."
-println "updateWarFile.groovy: invoking updateWarOnTomcat custom command ..."
-def service = context.waitForService(context.serviceName, 60, TimeUnit.SECONDS)
+println "updateWarFile.groovy: invoking ${command} custom command ..."
+def service = context.waitForService(HOST, 60, TimeUnit.SECONDS)
 def currentInstance = service.getInstances().find{ it.instanceId == context.instanceId }
-currentInstance.invoke("updateWarOnTomcat", warUrl as String, contextPath as String)
+currentInstance.invoke(command, "url=${warUrl}" as String, "contextPath=${contextPath}" as String)
 
 println "updateWarFile.groovy: End"
 return true

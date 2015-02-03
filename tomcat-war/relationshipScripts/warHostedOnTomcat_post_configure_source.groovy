047b165d23696233a48b31dbb9b2823aaea36687
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2015-02-03 13:58:30 +0100

    fixed tomcat sample for [ALIEN-703]  Artifacts paths provided to scripts are now absolute ones

--- tomcat-war/relationshipScripts/warHostedOnTomcat_post_configure_source.groovy
+++ tomcat-war/relationshipScripts/warHostedOnTomcat_post_configure_source.groovy
@@ -6,14 +6,13 @@ def context = ServiceContextFactory.getServiceContext()
 println "warHostedOnTomcat_post_configure_source<${SOURCE}> start."
 
 if (! war_file) return "warUrl is null. So we do nothing."
-def warUrl = "${context.serviceDirectory}/../${war_file}"
-println "warHostedOnTomcat_post_configure_source<${SOURCE}>: Target: ${TARGET_NAME}/${TARGET}:${tomcatIp}, Source: ${SOURCE_NAME}/${SOURCE}, warUrl is ${warUrl} and contextPath is ${contextPath}..."
+println "warHostedOnTomcat_post_configure_source<${SOURCE}>: Target: ${TARGET_NAME}/${TARGET}:${tomcatIp}, Source: ${SOURCE_NAME}/${SOURCE}, warUrl is ${war_file} and contextPath is ${contextPath}..."
 
 def command = "${TARGET_NAME}_updateWarOnTomcat"
 println "warHostedOnTomcat_post_configure_source<${SOURCE}> invoking ${command} custom command on target tomcat..."
 def service = context.waitForService(TARGET_SERVICE_NAME, 60, TimeUnit.SECONDS)
 def currentInstance = service.getInstances().find{ it.instanceId == context.instanceId }
-currentInstance.invoke(command, "url=${warUrl}" as String, "contextPath=${contextPath}" as String)
+currentInstance.invoke(command, "url=${war_file}" as String, "contextPath=${contextPath}" as String)
 println "warHostedOnTomcat_post_configure_source<${SOURCE}> end"
 
 return true

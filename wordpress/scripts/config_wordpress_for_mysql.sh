456b24e0e65bb6633c324912e420e000beac6774
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-02-24 11:31:00 +0100

    Fix changes not reported to cloudify 3

--- wordpress/scripts/config_wordpress_for_mysql.sh
+++ wordpress/scripts/config_wordpress_for_mysql.sh
@@ -9,4 +9,4 @@ sudo cp wp-config-sample.php wp-config.php
 sudo sed -i 's/database_name_here/'$DB_NAME'/' wp-config.php
 sudo sed -i 's/username_here/'$DB_USER'/' wp-config.php
 sudo sed -i 's/password_here/'$DB_PASSWORD'/' wp-config.php
-sudo sed -i 's/localhost/'$DB_IP'/' wp-config.php
+sudo sed -i 's/localhost/'$DB_IP:$DB_PORT'/' wp-config.php

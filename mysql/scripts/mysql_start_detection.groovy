902eb5740d839230e7c73107a0e85b2cd0a5a520
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-04-01 18:33:35 +0200

    Fix directory artifact access

--- mysql/scripts/mysql_start_detection.groovy
+++
@@ -1,6 +0,0 @@
-import org.cloudifysource.dsl.utils.ServiceUtils
-
-println "mysql_start_detection.groovy: port http=${PORT} ..."
-def isPortOccupied = ServiceUtils.isPortOccupied(Integer.parseInt(PORT))
-println "mysql_start_detection.groovy: isPortOccupied http=${PORT} ... ${isPortOccupied}"
-return isPortOccupied

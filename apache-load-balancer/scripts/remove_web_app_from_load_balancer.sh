c7902b8a8f65400bcd7223788269e176ec9f56c9
vuminhkh <minh.khang.vu.mkv@gmail.com>
2015-07-07 18:41:19 +0200

    Bug remove target

--- apache-load-balancer/scripts/remove_web_app_from_load_balancer.sh
+++ apache-load-balancer/scripts/remove_web_app_from_load_balancer.sh
@@ -5,7 +5,7 @@ PROXY_BALANCER_CONF_FILE=/etc/apache2/conf.d/proxy-balancer
 if [ ! -d "/etc/apache2/conf.d" ]; then
   PROXY_BALANCER_CONF_FILE=/etc/apache2/conf-enabled/proxy-balancer.conf
 fi
-
-sudo sed -i "/BalancerMember $WEB_APPLICATION_URL/d" $PROXY_BALANCER_CONF_FILE
+WEB_APPLICATION_URL_ESCAPED=$(echo $WEB_APPLICATION_URL | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')
+sudo sed -i "/BalancerMember $WEB_APPLICATION_URL_ESCAPED/d" $PROXY_BALANCER_CONF_FILE
 
 sudo /etc/init.d/apache2 restart

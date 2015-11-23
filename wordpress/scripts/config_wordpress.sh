6e9a0f90ee474774c3cc1fb361c88242b9fdbbaf
Philippe Benoit <philippe.benoit@fastconnect.fr>
2015-11-23 10:40:10 +0100

    Update Mysql, Wordpress it's topology to use the property by inheritance

--- wordpress/scripts/config_wordpress.sh
+++ wordpress/scripts/config_wordpress.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
-if [ "$CONTEXT_PATH" == "/" ]; then
-  CONTEXT_PATH=""
+if [ "$CONTEXT_ROOT" == "/" ]; then
+  CONTEXT_ROOT=""
 fi
 
-if [ ! -d $DOC_ROOT/$CONTEXT_PATH ]; then
-  eval "sudo mkdir -p $DOC_ROOT/$CONTEXT_PATH"
+if [ ! -d $DOC_ROOT/$CONTEXT_ROOT ]; then
+  eval "sudo mkdir -p $DOC_ROOT/$CONTEXT_ROOT"
 fi
 
-sudo rm -rf $DOC_ROOT/$CONTEXT_PATH/*
-sudo mv -f /opt/wordpress/wordpress/* $DOC_ROOT/$CONTEXT_PATH
-sudo chown -R www-data:www-data $DOC_ROOT/$CONTEXT_PATH
-sudo chmod 777 -R $DOC_ROOT/$CONTEXT_PATH
+sudo rm -rf $DOC_ROOT/$CONTEXT_ROOT/*
+sudo mv -f /opt/wordpress/wordpress/* $DOC_ROOT/$CONTEXT_ROOT
+sudo chown -R www-data:www-data $DOC_ROOT/$CONTEXT_ROOT
+sudo chmod 777 -R $DOC_ROOT/$CONTEXT_ROOT
 
 echo "End of Wordpress install, restart apache2 to charge all modules"
 sudo /etc/init.d/apache2 restart

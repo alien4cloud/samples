31e16c1d2da9f27220b71430c52855e331b9b38b
Igor Ngouagna <igor.ngouagna@fastconnect.fr>
2016-02-03 16:26:00 +0100

    host-pool-service: changed cofig script from python to full sh

--- host-pool-service/scripts/configure.sh
+++ host-pool-service/scripts/configure.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-echo "copying and unzipping the config folder from $pool_config to $WORK_DIR"
+echo "unzipping the config folder from $pool_config to $WORK_DIR"
 
 tar xzvf $pool_config -C $WORK_DIR
-ls ${WORK_DIR}
-COMMAND="python ${scripts}/configure.py $WORK_DIR $pool_config $POOL_FILE_NAME"
-echo "launching command $COMMAND"
-$COMMAND
+
+JSON_CONDIF_FILE="${WORK_DIR}/config.json"
+
+echo  "generating json config to  $JSON_CONDIF_FILE"
+echo -e "{\n  'pool': ${WORK_DIR}/${POOL_FILE_NAME}\n}" > $JSON_CONDIF_FILE

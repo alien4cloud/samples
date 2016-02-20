41c9c754f07c4b5b62e3fb12b8b56edbf364170e
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-02-20 17:03:27 +0100

    Use 'cat' instead of 'more' as more is interactive

--- elasticsearch/scripts/pre_configure_target.sh
+++ elasticsearch/scripts/pre_configure_target.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 sudo sed -i -e "s/discovery\.zen\.ping\.unicast\.hosts\: \[\(.*\)\]/discovery\.zen\.ping\.unicast\.hosts\: \[\1, \"$ES_IP\"\]/g" /etc/elasticsearch/elasticsearch.yml
-export i=`more /etc/elasticsearch/elasticsearch.yml | grep index.number_of_replicas | grep -v "#" | cut -d":" -f2 | xargs`
+export i=`cat /etc/elasticsearch/elasticsearch.yml | grep index.number_of_replicas | grep -v "#" | cut -d":" -f2 | xargs`
 export j=$((10#$i + 1))
 sudo sed -i -e "s/index\.number_of_replicas\: $i/index\.number_of_replicas\: $j/g" /etc/elasticsearch/elasticsearch.yml

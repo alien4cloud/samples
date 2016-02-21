387341d27bf6901a65130161c10ca3e57d910354
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-02-21 20:53:19 +0100

    Fix random deployment failure due to apache not restarted after php files copy

---
+++ demo-lifecycle/scripts/Registry/start.sh
@@ -0,0 +1,8 @@
+service="apache2"
+# Restart apache for php paths copied from create.sh taken into account
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+  sudo /etc/init.d/$service restart
+else
+  sudo /etc/init.d/$service start
+fi

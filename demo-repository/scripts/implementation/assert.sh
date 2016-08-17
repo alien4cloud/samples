22867f9a7056a1690a334048efafdf0cf5554c37
Philippe Benoit <philippe.benoit@fastconnect.fr>
2016-08-17 15:08:53 +0200

    Update demo-repository

--- demo-repository/scripts/implementation/assert.sh
+++ demo-repository/scripts/implementation/assert.sh
@@ -4,6 +4,7 @@ echo "I'm a remote script"
 
 if [ -z "$http_artifact" ]; then
     echo "remote_artifact is not set"
+    touch /var/www/artifact
     exit 1
 else
     echo "I have access to the artifact http_artifact at ${http_artifact}"
@@ -11,6 +12,7 @@ fi
 
 if [ -z "$git_artifact" ]; then
     echo "git_artifact is not set"
+    sudo touch /var/www/git
     exit 1
 else
     echo "I have access to the artifact git_artifact at ${git_artifact}"

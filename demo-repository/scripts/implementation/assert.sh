dc215b6426b3082c5fa0ade3f3bd948de5e79b48
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-08-23 16:44:37 +0200

    ALIEN-1611 : As a USER I want alien to support TOSCA repositories and to be able to fetch artifact from remote repositories. Add maven repo

--- demo-repository/scripts/implementation/assert.sh
+++ demo-repository/scripts/implementation/assert.sh
@@ -17,3 +17,11 @@ else
     echo "I have access to the artifact git_artifact at ${git_artifact}"
     sudo touch /var/www/git
 fi
+
+if [ -z "$maven_artifact" ]; then
+    echo "maven_artifact is not set"
+    exit 1
+else
+    echo "I have access to the artifact maven_artifact at ${maven_artifact}"
+    sudo touch /var/www/maven
+fi

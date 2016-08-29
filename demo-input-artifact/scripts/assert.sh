9fa5ebfb94a9a9a5e93410d20f616cb6c1d3a5bf
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2016-08-29 17:43:25 +0200

    Demo input artifact

---
+++ demo-input-artifact/scripts/assert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$local_war" ]; then
+    echo "local_war is not set"
+    exit 1
+else
+    echo "local_war is ${local_war}"
+fi
+
+if [ -z "$uploaded_war" ]; then
+    echo "uploaded_war is not set"
+    exit 1
+else
+    echo "uploaded_war is ${uploaded_war}"
+fi
+
+if [ -z "$remote_war" ]; then
+    echo "remote_war is not set"
+    exit 1
+else
+    echo "remote_war is ${remote_war}"
+fi

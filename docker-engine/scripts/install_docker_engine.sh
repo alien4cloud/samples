ccc8d3b197e2755bf9aa6c5ae8de8d4d1d42414c
afraisse <adrian.fraisse@gmail.com>
2016-09-09 17:43:22 +0200

    Separated hosting and docker cli capabilities

--- docker-engine/scripts/install_docker_engine.sh
+++ docker-engine/scripts/install_docker_engine.sh
@@ -24,8 +24,7 @@ case $OS in
         sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
         linux_codename=$(lsb_release -cs)
         echo "deb https://apt.dockerproject.org/repo ubuntu-${linux_codename} main" | sudo tee -a /etc/apt/sources.list.d/docker.list >/dev/null
-        sudo apt-get update
-        sudo apt-get purge -y lxc-docker
+        sudo apt-get -y update || (sleep 15; sudo apt-get update || exit $1)
         sudo apt-get install -y linux-image-extra-$(uname -r)
         sudo apt-get install -y docker-engine
 
@@ -44,7 +43,7 @@ EOF
         # Install
         sudo yum -y install docker-engine
         ;;
-        *)
+      *)
         echo "${OS} is not supported ATM. Exiting..."
         exit 1
         ;;

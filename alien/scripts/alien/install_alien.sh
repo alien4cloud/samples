a8a855afae797c529d19225553b5e9811e85e393
Luc Boutier <luc@fastconnect.fr>
2017-03-22 17:48:24 +0100

    - Migration of samples to latest dsl  - Renaming of types to follow a better naming convention (org.alien4cloud prefix)  - Improved compliance with TOSCA simple profile grammar and types (LoadBalancer compliance, etc.)  - Better coherency of various components.

--- alien/scripts/alien/install_alien.sh
+++ alien/scripts/alien/install_alien.sh
@@ -1,6 +1,31 @@
 #!/bin/bash -e
 
-echo "Using apt-get. Installing Alien4Cloud on one of the following : Debian, Ubuntu, Mint"
+echo "Installing Alien4Cloud."
+
+if [ -f /usr/bin/wget ]; then
+	DOWNLOADER="wget"
+elif [ -f /usr/bin/curl ]; then
+	DOWNLOADER="curl"
+fi
+
+# args:
+# $1 download description.
+# $2 download link.
+# $3 output file.
+download () {
+	echo "${currHostName}:${currFilename} Downloading $1 from $2 ..."
+	if [ "$DOWNLOADER" = "wget" ];then
+		Q_FLAG="--no-check-certificate"
+		O_FLAG="-O"
+		LINK_FLAG=""
+	elif [ "$DOWNLOADER" = "curl" ];then
+		Q_FLAG="-k"
+		O_FLAG="-o"
+		LINK_FLAG="-O"
+	fi
+	echo "${currHostName}:${currFilename} $DOWNLOADER $4 $Q_FLAG $O_FLAG $3 $LINK_FLAG $2"
+	sudo $DOWNLOADER $Q_FLAG $O_FLAG $3 $LINK_FLAG $2 || error_exit $? "Failed downloading $1"
+}
 
 # create user
 sudo useradd alien4cloud
@@ -11,11 +36,12 @@ if [ ! -d /var/log/alien4cloud ]; then
 fi
 
 # create application folder and copie files
-sudo wget --quiet -O alien.war ${APPLICATION_URL}
 if [ ! -d /opt/alien4cloud ]; then
   sudo mkdir /opt/alien4cloud
 fi
-sudo cp alien.war /opt/alien4cloud/alien.war
+
+echo "${currHostName}:${currFilename} Downloading ${APPLICATION_URL} to /opt/alien4cloud/alien.war ..."
+download "Alien4cloud" $APPLICATION_URL /opt/alien4cloud/alien.war
 
 # add configs
 if [ ! -d /etc/alien4cloud ]; then
--- apache-load-balancer/scripts/add_web_app_to_load_balancer.sh
+++ apache-load-balancer/scripts/add_web_app_to_load_balancer.sh
@@ -6,6 +6,8 @@ if [ ! -d "/etc/apache2/conf.d" ]; then
   PROXY_BALANCER_CONF_FILE=/etc/apache2/conf-enabled/proxy-balancer.conf
 fi
 
+WEB_APPLICATION_URL="$PROTOCOL://$IP:$PORT//$URL_PATH"
+
 sudo sed -i "/<Proxy balancer:\/\/mycluster>/a BalancerMember $WEB_APPLICATION_URL" $PROXY_BALANCER_CONF_FILE
 
 sudo /etc/init.d/apache2 restart

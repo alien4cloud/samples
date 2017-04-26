ddaa94b9412c5f3d9bd8b77be50f52981f2d5eed
Luc Boutier <luc@fastconnect.fr>
2017-04-26 12:36:17 +0200

    Fixing nodes, node cellar and mongo.

--- mongo/scripts/install-mongo.sh
+++ mongo/scripts/install-mongo.sh
@@ -2,6 +2,16 @@
 
 echo "BEGIN mongo install"
 
+if [ -e /proc/1/cgroup ] ; then
+  if grep docker /proc/1/cgroup -qa; then
+     # docker messes up the init system so we have to perform specific init
+     dpkg-divert --local --rename --add /sbin/initctl
+     ln -s /bin/true /sbin/initctl
+     dpkg-divert --local --rename --add /etc/init.d/mongod
+     ln -s /bin/true /etc/init.d/mongod
+  fi
+fi
+
 echo "add mongo key to source list"
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
@@ -12,7 +22,7 @@ echo "apt-get update"
 sudo apt-get update -y
 
 echo "apt-get install -y mongodb-org"
-sudo apt-get install -y mongodb-org
+sudo apt-get install -yq mongodb-org
 
 
 COUNTER=0

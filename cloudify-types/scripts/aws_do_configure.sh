026353c9e5eb8285657057ef1cfe17b5544b2041
Luc Boutier <luc@fastconnect.fr>
2017-05-02 14:34:09 +0200

    Fix manager configuration.

--- cloudify-types/scripts/aws_do_configure.sh
+++ cloudify-types/scripts/aws_do_configure.sh
@@ -11,6 +11,14 @@ if ! grep -q agent_private_key_path "$HOME_DIR/cfy_config_aws.yml"; then
   echo "agent_private_key_path: '$HOME_DIR/cfy_keys/$SSH_KEY_FILENAME'" >> "$HOME_DIR/cfy_config_aws.yml"
 fi
 
-sudo python ${python_script} -c "$HOME_DIR/cfy_config_aws.yml" -u $ADMIN_USERNAME -p $ADMIN_PASSWORD --ssl
+if [ ! -d /etc/cloudify/aws_plugin ]; then
+  mkdir /etc/cloudify/aws_plugin;
+fi
+
+if [ $MANAGER_PORT == 80 ]; then
+  sudo /opt/manager/env/bin/python ${python_script} -c "$HOME_DIR/cfy_config_aws.yml" -u $ADMIN_USERNAME -p $ADMIN_PASSWORD
+else
+  sudo /opt/manager/env/bin/python ${python_script} -c "$HOME_DIR/cfy_config_aws.yml" -u $ADMIN_USERNAME -p $ADMIN_PASSWORD --ssl
+fi
 
 echo "AWS configured"

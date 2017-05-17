79548b00a2439ac31e101c6cc9409d2fe93fa8ff
Victor Keophila <victor.keophila@fastconnect.fr>
2017-05-17 12:36:59 +0200

    Simplify petlcinic war scripts

--- petclinic-war/scripts/install_petclinic_mysql.sh
+++ petclinic-war/scripts/install_petclinic_mysql.sh
@@ -1,18 +1,5 @@
 #!/bin/sh -x
 
-# Wait petclinic to be deployed
-max_retries=10
-retries=0
-PETCLINIC_DIR=/opt/tomcat/webapps/$CONTEXT_PATH
-while [ ! -d "$PETCLINIC_DIR" -a $retries -lt $max_retries ] ; do
-  echo "Waiting petclinic. Wait and retry ($retries/$max_retries)"
-  sleep 10
-  retries=$(($retries+1))
-done
-
-# Override the initDB.sql script (the original script creates DB & Users but current user don't have the rights)
-sudo cp -r $mysql_dir/* $PETCLINIC_DIR/WEB-INF/classes/db/mysql
-
 # Setup MySql/JDBC Connector properties
 TOMCAT_SETENV_FILE=/opt/tomcat/bin/setenv.sh
 
@@ -36,5 +23,11 @@ echo "export CATALINA_OPTS=\"\$CATALINA_OPTS -Djdbc.initLocation=classpath:db/my
 
 # Restart tomcat to read the JDBC properties
 sudo /opt/tomcat/bin/shutdown.sh
-sleep 5
+
+# Ensure that tomcat is really shutdown
+while [ $(ps -ef | grep -v grep | grep catalina | wc -l) -ne 0 ] ; do
+  echo "Ending catalina process... "
+  sleep 2
+done
+
 sudo /opt/tomcat/bin/startup.sh
--- petclinic-war/scripts/install_petclinic_postgresql.sh
+++ petclinic-war/scripts/install_petclinic_postgresql.sh
@@ -1,19 +1,5 @@
 #!/bin/sh -x
 
-# wait petclinic to be deployed
-max_retries=10
-retries=0
-PETCLINIC_DIR=/opt/tomcat/webapps/$CONTEXT_PATH
-while [ ! -d "$PETCLINIC_DIR" -a $retries -lt $max_retries ] ; do
-  echo "Waiting petclinic. Wait and retry ($retries/$max_retries)"
-  sleep 10
-  retries=$(($retries+1))
-done
-
-# Prepare petclinic (FIXME)
-sudo curl https://jdbc.postgresql.org/download/postgresql-42.0.0.jre7.jar -o $PETCLINIC_DIR/WEB-INF/lib/postgresql-42.0.0.jre7.jar 
-sudo cp -r $postgresql_dir $PETCLINIC_DIR/WEB-INF/classes/db/postgresql
-
 # Configure JDBC Driver
 TOMCAT_SETENV_FILE=/opt/tomcat/bin/setenv.sh
 
@@ -36,5 +22,11 @@ fi
 
 # Restart tomcat to read the JDBC properties
 sudo /opt/tomcat/bin/shutdown.sh
-sleep 5
+
+# Ensure that tomcat is really shutdown
+while [ $(ps -ef | grep -v grep | grep catalina | wc -l) -ne 0 ] ; do
+  echo "Ending catalina process... "
+  sleep 2
+done
+
 sudo /opt/tomcat/bin/startup.sh

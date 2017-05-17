6c896436ecda07a765a2deee7391ee9e8833c80e
Guillaume Vaudaux-Ruth <guillaume.vaudauxruth@fastconnect.fr>
2017-05-17 10:16:48 +0200

    Fix: war is not deployed because of missing .war extension

--- tomcat-war/scripts/tomcat_install_war.sh
+++ tomcat-war/scripts/tomcat_install_war.sh
@@ -34,6 +34,10 @@ download () {
 }
 
 if [ -z "${WAR_URL}" ];then
+	# The war_file MUST have the .war extension otherwise it won't work
+	mv ${war_file} ${war_file}.war
+	war_file=${war_file}.war
+
     echo "Use deployment artifact from ${war_file}"
 else
     echo "Override deployment artifact ${war_file} with war from ${WAR_URL}"

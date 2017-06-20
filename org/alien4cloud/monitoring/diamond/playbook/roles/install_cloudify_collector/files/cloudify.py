bb3a3b921cd5e0ec2bcb8a2614e08c843492df5b
Xavier DEGENNE <xavier@fastconnect.fr>
2017-06-20 07:38:06 +0200

    Add a cry patch that increase max open file for cfyuser Refactor patches Add different measures to cfy diamond collector

--- org/alien4cloud/monitoring/diamond/playbook/roles/install_cloudify_collector/files/cloudify.py
+++ org/alien4cloud/monitoring/diamond/playbook/roles/install_cloudify_collector/files/cloudify.py
@@ -6,9 +6,9 @@ import tempfile
 
 class CloudifyCollector(diamond.collector.Collector):
 
-    def proc_count(self, name):
+    def proc_count(self, cmd):
         filename = tempfile.mktemp()
-        os.system("ps -ef | grep " + name + " | wc -l > " + filename)
+        os.system(cmd + " > " + filename)
         count = open(filename).read().strip()
         os.remove(filename)
         return int(count)
@@ -32,7 +32,21 @@ class CloudifyCollector(diamond.collector.Collector):
     def collect(self):
 
         metric_name = "cloudify.mgmtworker.count"
-        metric_value = self.proc_count("mgmtworker")
+        metric_value = self.proc_count("ps -ef | grep mgmtworker | grep -v grep | wc -l")
+        self.publish(metric_name, metric_value)
 
-        # Publish Metric
+        metric_name = "cloudify.cloudify_hostpool_plugin.count"
+        metric_value = self.proc_count("ps -ef | grep cloudify_hostpool_plugin | grep -v grep | wc -l")
+        self.publish(metric_name, metric_value)
+
+        metric_name = "cloudify.task-plugin.count"
+        metric_value = self.proc_count("ps -ef | grep task-plugin | grep -v grep | wc -l")
+        self.publish(metric_name, metric_value)
+
+        metric_name = "cloudify.task-script_runner.count"
+        metric_value = self.proc_count("ps -ef | grep task-script_runner | grep -v grep | wc -l")
+        self.publish(metric_name, metric_value)
+
+        metric_name = "cloudify.celery.count"
+        metric_value = self.proc_count("ps -ef | grep celery | grep -v grep | wc -l")
         self.publish(metric_name, metric_value)

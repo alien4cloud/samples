19062d7a98a083561506ec9fdca91d6b33acccbd
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2017-08-02 12:11:04 +0200

    Fix service demi relationship

--- apache-load-balancer/scripts/service_add_web_app_to_load_balancer.sh
+++ apache-load-balancer/scripts/service_add_web_app_to_load_balancer.sh
@@ -4,7 +4,7 @@ export ADD_SCRIPT="${script_directory}/add_web_app_to_load_balancer.sh"
 
 echo "Execute load balancer add script $ADD_SCRIPT on remote host: ssh -i ${ssh_key} ${USER}@${A4C_EXECUTION_HOST}"
 
-sudo chmod 600 ${ssh_key}
+chmod 0400 ${ssh_key}
 
 # Just execute the add_web_app_to_load_balancer script on the target machine.
 ssh -o StrictHostKeyChecking=no -i ${ssh_key} ${USER}@${A4C_EXECUTION_HOST} PROTOCOL=$PROTOCOL IP=$IP PORT=$PORT URL_PATH=$URL_PATH 'bash -s' < ${ADD_SCRIPT}
--- apache-load-balancer/scripts/service_remove_web_app_from_load_balancer.sh
+++ apache-load-balancer/scripts/service_remove_web_app_from_load_balancer.sh
@@ -4,7 +4,7 @@ export REMOVE_SCRIPT="${script_directory}/remove_web_app_from_load_balancer.sh"
 
 echo "Execute load balancer add script $REMOVE_SCRIPT on remote host: ssh -i ${ssh_key} ${USER}@${A4C_EXECUTION_HOST}"
 
-sudo chmod 600 ${ssh_key}
+chmod 600 ${ssh_key}
 
 # Just execute the add_web_app_to_load_balancer script on the target machine.
 ssh -o StrictHostKeyChecking=no -i ${ssh_key} ${USER}@${A4C_EXECUTION_HOST} PROTOCOL=$PROTOCOL IP=$IP PORT=$PORT URL_PATH=$URL_PATH 'bash -s' < ${REMOVE_SCRIPT}

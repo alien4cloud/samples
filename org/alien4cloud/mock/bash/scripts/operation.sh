c5af8b62fdd82e18a27c9d2fdae894b1a922ad44
Xavier DEGENNE <xavier.degenne@gmail.com>
2019-03-07 14:23:41 +0100

    add attributes to bashmock type

--- org/alien4cloud/mock/bash/scripts/operation.sh
+++ org/alien4cloud/mock/bash/scripts/operation.sh
@@ -3,3 +3,13 @@ echo "${NODE}.${duration} sleep: ${duration} sec"
 /bin/sleep $duration
 
 /bin/more ${data}
+
+export ATT_HOSTNAME=$HOSTNAME
+export ATT_DATE=$(date)
+export ATT_SSH_CONNECTION=$SSH_CONNECTION
+export ATT_CURRENT_PID=$$
+export ATT_HOME=$ATT_HOME
+export ATT_PWD=$(pwd)
+export ATT_LOGNAME=$LOGNAME
+export ATT_SSH_CLIENT=$SSH_CLIENT
+export ATT_PPID=$PPID

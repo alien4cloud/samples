0d4fd783c11e004a0b48a9c115f6b9603f609775
Sébastien LAURENT <sebastien.laurent@atos.net>
2020-01-06 12:18:10 +0100

    Working run scripts for job

--- org/alien4cloud/mock/jobs/scripts/run-ko.sh
+++ org/alien4cloud/mock/jobs/scripts/run-ko.sh
@@ -1,3 +1,22 @@
-#!/bin/bash -e
-echo "Running Job <$TOSCA_JOB_ID> that should fail"
-export TOSCA_JOB_STATUS="FAILED"
+#!/bin/bash
+
+FILE=/tmp/$TOSCA_JOB_ID
+
+if [ -f "$FILE" ]; then
+        num=$(<"$FILE")
+        num=$(( num+1 ))
+else
+        num=1
+        echo "1" > $FILE
+fi
+echo $num > $FILE
+
+if (( num >= 10 )); then
+        echo "Done"
+        rm $FILE
+		export TOSCA_JOB_STATUS="FAILED"
+else
+        echo "Wait (id=$TOSCA_JOB_ID count=$num)"
+        export TOSCA_JOB_STATUS="RUNNING"
+fi
+
--- org/alien4cloud/mock/jobs/scripts/run-ok.sh
+++ org/alien4cloud/mock/jobs/scripts/run-ok.sh
@@ -1,6 +1,22 @@
-#!/bin/bash -e
-echo "Running Job <$TOSCA_JOB_ID> that should success (sleeping $duration sec)"
+#!/bin/bash
 
-/bin/sleep $duration
+FILE=/tmp/$TOSCA_JOB_ID
 
-export TOSCA_JOB_STATUS="COMPLETED"
+if [ -f "$FILE" ]; then
+        num=$(<"$FILE")
+        num=$(( num+1 ))
+else
+        num=1
+        echo "1" > $FILE
+fi
+echo $num > $FILE
+
+if (( num >= 10 )); then
+        echo "Done"
+        rm $FILE
+		export TOSCA_JOB_STATUS="COMPLETED"
+else
+        echo "Wait (id=$TOSCA_JOB_ID count=$num)"
+        export TOSCA_JOB_STATUS="RUNNING"
+fi
+
